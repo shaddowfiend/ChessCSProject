5dcde8f75426c7e2530103b4787f48b175c5108d
betrayed <tihuno@mail.ru>
2023-12-20 00:00:48 +0200

    Optimized+TableColors

--- ChessCS/ChessTable.cs
+++ ChessCS/ChessTable.cs
@@ -15,46 +15,27 @@ namespace ChessCS
             {new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty) },
             {new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty),new Figures(Figures.Empty) }
         };
-        public void TableChange()  //Changing table while status is not checkmate
-        {
-            
-        }
         public void TableShow()
         {
-            for (int i = 0; i < table.GetLength(0); i++) 
+            for (int row = 0; row < table.GetLength(0); row++) 
             {
-                for (int j = 0; j < table.GetLength(1); j++) 
+                for (int col = 0; col < table.GetLength(1); col++) 
                 {
-                    Console.Write(table[i,j]);
-                }
-                Console.WriteLine();
-            }
-        }
-        /*private void DrawChessboard()
-        {
-            int rows = table.GetLength(0);
-            int cols = table.GetLength(1);
-
-            for (int row = 0; row < rows; row++)
-            {
-                for (int col = 0; col < cols; col++)
-                {
-                    string cellValue = table[row, col];
                     if ((row + col) % 2 == 0)
                     {
                         Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
                     }
                     else
                     {
                         Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.White;
                     }
-
-                    Console.Write(cellValue.PadRight(8));
+                    Console.Write(table[row, col]?.ToString() ?? Figures.Empty);
+                    Console.ResetColor();
                 }
-
-                Console.ResetColor();
-                Console.WriteLine(); 
+                Console.WriteLine();
             }
-        }*/
+        }
     }
 }
--- ChessCS/Figures.cs
+++ ChessCS/Figures.cs
@@ -2,13 +2,13 @@
 {
     public class Figures
     {
-        public static string Pawn = "  |p|  ";
-        public static string Knight = "  |N|  ";
-        public static string Bishop = "  |B|  ";
-        public static string Rook = "  |R|  ";
-        public static string Queen = "  |Q|  ";
-        public static string King = "  |K|  ";
-        public static string Empty = "   0   ";
+        public static string Pawn = " |p| ";
+        public static string Knight = " |N| ";
+        public static string Bishop = " |B| ";
+        public static string Rook = " |R| ";
+        public static string Queen = " |Q| ";
+        public static string King = " |K| ";
+        public static string Empty = "  0  ";
 
         public string Name { get; private set; }
 
